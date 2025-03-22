842c6937c98f0efbcb9108dc3a529d790e75cbba
Gaung Taqwa Indraswara <gaungswara44@gmail.com>
2025-03-22 18:47:43 +0700

    update settings and entrypoint

--- Back-End/djangocoffee_backend/djangocoffee_backend/settings.py
+++ Back-End/djangocoffee_backend/djangocoffee_backend/settings.py
@@ -88,7 +88,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -167,6 +167,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
